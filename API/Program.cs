a112d04fbb234c18a40fd59b1132437a306a500c
Jeremy Beyleveld <jbeyleveld@singular.co.za>
2025-02-10 08:32:21 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -19,7 +26,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-}
+    }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
